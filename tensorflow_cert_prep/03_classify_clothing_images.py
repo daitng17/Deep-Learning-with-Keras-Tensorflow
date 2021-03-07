74f473437b79418405e970f02a37100b28b73d9b
Nguyen Dai Truong Thanh <36881258+daitng17@users.noreply.github.com>
2021-03-07 12:30:51 +0900

    add exercise 03

--- tensorflow_cert_prep/03_classify_clothing_images.py
+++ tensorflow_cert_prep/03_classify_clothing_images.py
@@ -6,16 +6,19 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-print(tf.__version__)
+
+# print(tf.__version__)
 
 
 # define callback class
 class myCallback(keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
-        if logs.get('accuracy') > 0.95:
+        if logs.get('accuracy') > 0.90:
             print('\nReached 95% accuracy so cancelling training.')
             self.model.stop_training = True
 
+
+callback = myCallback()
 
 # prepare the data
 fashion_mnist = tf.keras.datasets.fashion_mnist
@@ -25,3 +28,55 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 # explore the data
+train_images.shape
+len(train_labels)
+test_images.shape
+len(test_labels)
+
+# data preprocessing
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
+
+# normalize the values
+# train_images = train_images.reshape(60000, 28, 28, 1)
+train_images = train_images / 255.0
+
+# test_images = test_images.reshape(10000, 28, 28, 1)
+test_images = test_images / 255.0
+
+# plt.figure(figsize=(10, 10))
+# for i in range(25):
+#     plt.subplot(5, 5, i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
+
+# build the model
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+# compile the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# train the model
+model.fit(train_images, train_labels, epochs=10, verbose=1, callbacks=[callback])
+
+# evaluate accuracy
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
+
+# make predictions
+predictions = model.predict(test_images)
+
+print(predictions[0])
+print(np.argmax(predictions[0]))
+print(test_labels[0])
